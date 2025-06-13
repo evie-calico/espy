520c9ed2734ec785de519483f5d377039c6be9b5
Evie <14899090+evie-calico@users.noreply.github.com>
2025-06-12 22:30:47 -0400

    Simple blocks

--- espy-tail/src/lib.rs
+++ espy-tail/src/lib.rs
@@ -7,6 +7,8 @@ pub mod instruction {
     // Stack primatives: 0x00-0x0F
     pub const CLONE: u8 = 0x00;
     pub const WRITE: u8 = 0x01;
+    pub const COLLAPSE: u8 = 0x02;
+    pub const JUMP: u8 = 0x03;
 
     // Push ops: 0x10-0x2F
     pub const PUSH_UNIT: u8 = 0x10;
@@ -19,12 +21,21 @@ pub mod instruction {
     pub const DIV: u8 = 0x33;
 }
 
+pub type StackPointer = u32;
+pub type BlockId = u32;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Instruction {
     /// Copy a value from the given position and put it on the top of the stack.
-    Clone(u32),
+    Clone(StackPointer),
     /// Pop a value off the stack and write it to the given position.
-    Write(u32),
+    Write(StackPointer),
+    /// Pop a value off the stack and write it to the given position.
+    /// Then, pop each value succeeding this position and discard it.
+    Collapse(StackPointer),
+    /// Using the current stack,
+    /// jump to the given block id and execute it.
+    Jump(BlockId),
 
     PushUnit,
     PushI64(i64),
@@ -58,6 +69,8 @@ impl Iterator for InstructionIter {
         let byte = match self.instruction {
             Instruction::Clone(from) => decompose!(instruction::CLONE, from as 1..=4),
             Instruction::Write(to) => decompose!(instruction::WRITE, to as 1..=4),
+            Instruction::Collapse(to) => decompose!(instruction::COLLAPSE, to as 1..=4),
+            Instruction::Jump(block_id) => decompose!(instruction::JUMP, block_id as 1..=4),
 
             Instruction::PushUnit => decompose!(instruction::PUSH_UNIT,),
             Instruction::PushI64(literal) => decompose!(instruction::PUSH_I64, literal as 1..=8),
@@ -106,91 +119,103 @@ impl Program {
         }
         output
     }
+
+    fn add_block<'source>(&mut self, block: Block<'source>) {
+        let block_id = self.blocks.len();
+        self.blocks.push(Vec::new());
+        let mut scope = Scope::default();
+        for i in block.statements {
+            let statement = self.add_statement(i, &mut scope);
+            self.blocks[block_id].extend(statement);
+        }
+        match block.result {
+            espy_ears::BlockResult::Expression(i) => {
+                let expression = self.add_expression(i, &scope);
+                self.blocks[block_id].extend(expression);
+            }
+            espy_ears::BlockResult::Function(_) => todo!(),
+        }
+    }
+
+    /// # Panic
+    ///
+    /// Panics if provided an invalid statement.
+    /// Statements should be validated beforehand by checking the `diagnostics.errors` field.
+    fn add_statement<'source>(
+        &mut self,
+        statement: Statement<'source>,
+        scope: &mut Scope<'source>,
+    ) -> Vec<Instruction> {
+        match statement.action {
+            Some(Action::Binding(Binding { ident_token, .. })) => {
+                // Expressions always put *one* item on the stack, so protect it by advancing sp.
+                scope.bindings.insert(
+                    ident_token.expect("invalid statement structure").origin,
+                    Value {
+                        index: scope.stack_pointer,
+                    },
+                );
+                scope.stack_pointer += 1;
+            }
+            Some(Action::Break(_)) => todo!(),
+            None => todo!(),
+        }
+        self.add_expression(statement.expression.unwrap_or_default(), scope)
+    }
+
+    fn add_expression(&mut self, expression: Expression, scope: &Scope) -> Vec<Instruction> {
+        if expression.contents.is_empty() {
+            return vec![Instruction::PushUnit];
+        }
+        expression
+            .contents
+            .into_iter()
+            .map(|node| {
+                match node {
+                    Node::Unit => Instruction::PushUnit,
+                    Node::Number(Token { origin, .. }) => {
+                        // TODO: Must be handled.
+                        let integer = origin.parse().expect("invalid i64");
+                        Instruction::PushI64(integer)
+                    }
+                    Node::Ident(Token { origin, .. }) => {
+                        // TODO: Must be handled.
+                        let value = scope.bindings.get(origin).expect("undefined symbol");
+                        Instruction::Clone(value.index)
+                    }
+                    Node::Add(_) => Instruction::Add,
+                    Node::Sub(_) => Instruction::Sub,
+                    Node::Mul(_) => Instruction::Mul,
+                    Node::Div(_) => Instruction::Div,
+                    Node::Block(block) => {
+                        let new_block = self.blocks.len();
+                        self.add_block(block);
+                        // TODO: Must be handled.
+                        Instruction::Jump(new_block.try_into().expect("block limit reached"))
+                    }
+                    _ => todo!(),
+                }
+            })
+            .collect()
+    }
 }
 
 impl<'source> From<Block<'source>> for Program {
     fn from(block: Block<'source>) -> Self {
-        let mut scope = Scope::default();
-        let mut root_block = Vec::new();
-        for i in block.statements {
-            root_block.extend(statement(i, &mut scope));
-        }
-        match block.result {
-            espy_ears::BlockResult::Expression(i) => root_block.extend(expression(i, &scope)),
-            espy_ears::BlockResult::Function(_) => todo!(),
-        }
-        Program {
-            blocks: vec![root_block],
-        }
+        let mut this = Self::default();
+        this.add_block(block);
+        this
     }
 }
 
 pub struct Value {
-    index: u32,
+    index: StackPointer,
 }
 
 #[derive(Default)]
 pub struct Scope<'source> {
-    stack_pointer: u32,
+    stack_pointer: StackPointer,
     bindings: HashMap<&'source str, Value>,
-}
-
-/// # Panic
-///
-/// Panics if provided an invalid statement.
-/// Statements should be validated beforehand by checking the `diagnostics.errors` field.
-fn statement<'source, 'scope>(
-    statement: Statement<'source>,
-    scope: &'scope mut Scope<'source>,
-) -> impl Iterator<Item = Instruction> + 'scope {
-    match statement.action {
-        Some(Action::Binding(Binding { ident_token, .. })) => {
-            // Expressions always put *one* item on the stack, so protect it by advancing sp.
-            scope.bindings.insert(
-                ident_token.expect("invalid statement structure").origin,
-                Value {
-                    index: scope.stack_pointer,
-                },
-            );
-            scope.stack_pointer += 1;
-        }
-        Some(Action::Break(_)) => todo!(),
-        None => todo!(),
-    }
-    expression(statement.expression.unwrap_or_default(), scope)
-}
-
-fn expression(expression: Expression, scope: &Scope) -> impl Iterator<Item = Instruction> {
-    assert!(
-        expression.diagnostics.errors.is_empty(),
-        "expression must be valid"
-    );
-    if expression.contents.is_empty() {
-        Some(Instruction::PushUnit)
-    } else {
-        None
-    }
-    .into_iter()
-    .chain(expression.contents.into_iter().flat_map(|node| {
-        match node {
-            Node::Unit => Some(Instruction::PushUnit),
-            Node::Number(Token { origin, .. }) => {
-                // TODO: Must be handled.
-                let integer = origin.parse().expect("invalid i64");
-                Some(Instruction::PushI64(integer))
-            }
-            Node::Ident(Token { origin, .. }) => {
-                // TODO: Must be handled.
-                let value = scope.bindings.get(origin).expect("undefined symbol");
-                Some(Instruction::Clone(value.index))
-            }
-            Node::Add(_) => Some(Instruction::Add),
-            Node::Sub(_) => Some(Instruction::Sub),
-            Node::Mul(_) => Some(Instruction::Mul),
-            Node::Div(_) => Some(Instruction::Div),
-            _ => todo!(),
-        }
-    }))
 }
 
 #[cfg(test)]
@@ -216,6 +241,7 @@ mod tests {
         let actual = program.compile();
         let expected = program![
             4u32,
+            // block 0
             instruction::PUSH_I64,
             1i64,
             instruction::PUSH_I64,
@@ -231,6 +257,28 @@ mod tests {
             instruction::CLONE,
             1u32,
             instruction::SUB,
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn simple_blocks() {
+        let mut lexer = Lexer::from("{ 1 + 2 }").peekable();
+        let block = Block::from(&mut lexer);
+        let program = Program::from(block);
+        let actual = program.compile();
+        let expected = program![
+            8u32,
+            13u32,
+            // block 0
+            instruction::JUMP,
+            1 as BlockId,
+            // block 1
+            instruction::PUSH_I64,
+            1i64,
+            instruction::PUSH_I64,
+            2i64,
+            instruction::ADD,
         ];
         assert_eq!(actual, expected);
     }
