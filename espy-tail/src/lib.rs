26751622a4b1937598541f3af5a0c7c8c5bb581e
Evie <14899090+evie-calico@users.noreply.github.com>
2025-06-12 23:47:14 -0400

    Function creation

--- espy-tail/src/lib.rs
+++ espy-tail/src/lib.rs
@@ -13,6 +13,7 @@ pub mod instruction {
     // Push ops: 0x10-0x2F
     pub const PUSH_UNIT: u8 = 0x10;
     pub const PUSH_I64: u8 = 0x11;
+    pub const PUSH_FUNCTION: u8 = 0x12;
 
     // Arithmatic ops: 0x30-0x4F
     pub const ADD: u8 = 0x30;
@@ -31,11 +32,17 @@ pub enum Instruction {
     /// Pop a value off the stack and write it to the given position.
     Write(StackPointer),
     /// Pop a value off the stack and write it to the given position.
-    /// Then, pop each value succeeding this position and discard it.
+    /// Then, pop each value above this position and discard it.
     Collapse(StackPointer),
     /// Using the current stack,
     /// jump to the given block id and execute it.
     Jump(BlockId),
+    /// Pop the top `captures` values off the stack and onto a new stack,
+    /// and then push a function containing the new stack and the proceeding block id to the current stack.
+    PushFunction {
+        captures: StackPointer,
+        function: BlockId,
+    },
 
     PushUnit,
     PushI64(i64),
@@ -74,6 +81,9 @@ impl Iterator for InstructionIter {
 
             Instruction::PushUnit => decompose!(instruction::PUSH_UNIT,),
             Instruction::PushI64(literal) => decompose!(instruction::PUSH_I64, literal as 1..=8),
+            Instruction::PushFunction { captures, function } => {
+                decompose!(instruction::PUSH_FUNCTION, captures as 1..=4, function as 5..=8)
+            }
 
             Instruction::Add => decompose!(instruction::ADD,),
             Instruction::Sub => decompose!(instruction::SUB,),
@@ -120,20 +130,32 @@ impl Program {
         output
     }
 
-    fn add_block<'source>(&mut self, block: Block<'source>, scope: &mut Scope<'source>) {
+    fn add_block<'source>(&mut self, block: Block<'source>, mut scope: Scope<'source>) -> BlockId {
         let block_id = self.blocks.len();
         self.blocks.push(Vec::new());
         for i in block.statements {
-            let statement = self.add_statement(i, scope);
+            let statement = self.add_statement(i, &mut scope);
             self.blocks[block_id].extend(statement);
         }
         match block.result {
             espy_ears::BlockResult::Expression(i) => {
-                let expression = self.add_expression(i, scope);
+                let expression = self.add_expression(i, &mut scope);
                 self.blocks[block_id].extend(expression);
             }
-            espy_ears::BlockResult::Function(_) => todo!(),
+            espy_ears::BlockResult::Function(function) => {
+                let mut scope = scope.promote();
+                let captures = scope.stack_pointer;
+                // Note that, while a function takes a single argument,
+                // the inner block recieves it destructured as expected by `with`.
+                for argument in function.arguments {
+                    scope.stack_pointer += 1;
+                    scope.insert(argument.origin);
+                }
+                let function = self.add_block(function.block, scope);
+                self.blocks[block_id].push(Instruction::PushFunction { captures, function })
+            }
         }
+        block_id.try_into().expect("block limit reached")
     }
 
     /// # Panic
@@ -148,13 +170,7 @@ impl Program {
         let instructions = self.add_expression(statement.expression.unwrap_or_default(), scope);
         match statement.action {
             Some(Action::Binding(Binding { ident_token, .. })) => {
-                scope.bindings.insert(
-                    ident_token.expect("invalid statement structure").origin,
-                    Value {
-                        // The expressions's result is under the stack pointer right now.
-                        index: scope.stack_pointer - 1,
-                    },
-                );
+                scope.insert(ident_token.expect("invalid statement structure").origin);
             }
             Some(Action::Break(_)) => todo!(),
             None => todo!(),
@@ -184,7 +200,7 @@ impl Program {
                     }
                     Node::Ident(Token { origin, .. }) => {
                         // TODO: Must be handled.
-                        let value = scope.bindings.get(origin).expect("undefined symbol");
+                        let value = scope.get(origin).expect("undefined symbol");
                         scope.stack_pointer += 1;
                         Instruction::Clone(value.index)
                     }
@@ -205,14 +221,10 @@ impl Program {
                         Instruction::Div
                     }
                     Node::Block(block) => {
-                        let new_block = self.blocks.len();
-                        self.add_block(block, scope);
-                        println!("{scope:?}");
-                        scope.free();
-                        println!("{scope:?}");
+                        let new_block = self.add_block(block, scope.child());
                         scope.stack_pointer += 1;
                         // TODO: Must be handled.
-                        Instruction::Jump(new_block.try_into().expect("block limit reached"))
+                        Instruction::Jump(new_block)
                     }
                     _ => todo!(),
                 }
@@ -224,26 +236,56 @@ impl Program {
 impl<'source> From<Block<'source>> for Program {
     fn from(block: Block<'source>) -> Self {
         let mut this = Self::default();
-        this.add_block(block, &mut Scope::default());
+        this.add_block(block, Scope::default());
         this
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct Value {
     index: StackPointer,
 }
 
 #[derive(Debug, Default)]
 pub struct Scope<'source> {
+    parent: Option<&'source Scope<'source>>,
     stack_pointer: StackPointer,
     bindings: HashMap<&'source str, Value>,
 }
 
-impl Scope<'_> {
-    fn free(&mut self) {
+impl<'source> Scope<'source> {
+    fn get(&self, k: &'source str) -> Option<Value> {
         self.bindings
-            .retain(|_, binding| binding.index < self.stack_pointer);
+            .get(k)
+            .copied()
+            .or_else(|| self.parent.and_then(|parent| parent.get(k)))
+    }
+
+    fn insert(&mut self, k: &'source str) {
+        self.bindings.insert(
+            k,
+            Value {
+                index: self.stack_pointer - 1,
+            },
+        );
+    }
+
+    fn child(&'source self) -> Self {
+        Self {
+            parent: Some(self),
+            stack_pointer: self.stack_pointer,
+            bindings: HashMap::new(),
+        }
+    }
+
+    fn promote(mut self) -> Self {
+        let lost_size = self.parent.map_or(0, |x| x.stack_pointer);
+        self.parent = None;
+        self.stack_pointer -= lost_size;
+        for binding in &mut self.bindings {
+            binding.1.index -= lost_size;
+        }
+        self
     }
 }
 
@@ -314,6 +356,31 @@ mod tests {
             0 as StackPointer,
             instruction::CLONE,
             2 as StackPointer,
+            instruction::MUL,
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn function_creation() {
+        let mut lexer = Lexer::from("let x = 2; with y; x * y").peekable();
+        let block = Block::from(&mut lexer);
+        let program = Program::from(block);
+        let actual = program.compile();
+        let expected = program![
+            8u32,
+            26u32,
+            // block 0
+            instruction::PUSH_I64,
+            2i64,
+            instruction::PUSH_FUNCTION,
+            1 as StackPointer,
+            1 as BlockId,
+            // block 1
+            instruction::CLONE,
+            0 as StackPointer,
+            instruction::CLONE,
+            1 as StackPointer,
             instruction::MUL,
         ];
         assert_eq!(actual, expected);
