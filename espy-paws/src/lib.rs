01d0a928304e1e4b7bb46399b083f00e1955c66f
Evie <14899090+evie-calico@users.noreply.github.com>
2025-09-04 16:08:46 -0400

    Merge espy-paws files

--- espy-paws/src/lib.rs
+++ espy-paws/src/lib.rs
@@ -1,11 +1,7 @@
+use espy_heart::prelude::*;
 use std::cell::RefCell;
 use std::mem;
 use std::rc::{Rc, Weak};
-
-mod interpreter;
-pub use interpreter::*;
-mod interop;
-pub use interop::{Extern, ExternFn, FunctionWrapper, FunctionWrapperMut, wrap_fn, wrap_fn_mut};
 
 fn rc_slice_try_from_iter<T, E>(
     len: usize,
@@ -1286,5 +1282,442 @@ pub enum InvalidBytecode {
 impl<'host> From<InvalidBytecode> for Error<'host> {
     fn from(e: InvalidBytecode) -> Error<'host> {
         Error::InvalidBytecode(e)
+    }
+}
+
+/// Reads a u32 and casts it to usize for convenience.
+fn read_header(bytes: &[u8], at: usize) -> Result<usize, InvalidBytecode> {
+    let (((a, b), c), d) = bytes
+        .get(at)
+        .zip(bytes.get(at + 1))
+        .zip(bytes.get(at + 2))
+        .zip(bytes.get(at + 3))
+        .ok_or(InvalidBytecode::MalformedHeader)?;
+    Ok(u32::from_le_bytes([*a, *b, *c, *d]) as usize)
+}
+
+fn block_count(bytes: &[u8]) -> Result<usize, InvalidBytecode> {
+    read_header(bytes, 0)
+}
+
+fn string_count(bytes: &[u8]) -> Result<usize, InvalidBytecode> {
+    read_header(bytes, size_of::<u32>())
+}
+
+fn offsets(bytes: &[u8]) -> Result<&[u8], InvalidBytecode> {
+    let offset_count = block_count(bytes)? + string_count(bytes)?;
+    let first_offset = size_of::<u32>() * 2;
+    let last_offset = first_offset + size_of::<u32>() * offset_count;
+    bytes
+        .get(first_offset..last_offset)
+        .ok_or(InvalidBytecode::MalformedHeader)
+}
+
+fn block(bytes: &[u8], block_id: usize) -> Result<&[u8], InvalidBytecode> {
+    let offsets = offsets(bytes)?;
+    let block_position = size_of::<u32>() * block_id;
+    let start = read_header(offsets, block_position)?;
+    let end = read_header(offsets, size_of::<u32>() + block_position).unwrap_or(bytes.len());
+    bytes
+        .get(start..end)
+        .ok_or(InvalidBytecode::MalformedHeader)
+}
+
+#[derive(Clone, Debug)]
+pub struct Program {
+    pub(crate) bytes: Rc<[u8]>,
+    owned_strings: Rc<[Rc<str>]>,
+}
+
+impl TryFrom<Rc<[u8]>> for Program {
+    type Error = Error<'static>;
+
+    fn try_from(bytes: Rc<[u8]>) -> Result<Self, Self::Error> {
+        let string_count = string_count(&bytes)?;
+        let owned_strings = (0..string_count)
+            .map(|string| {
+                let string = size_of::<u32>() * (string + block_count(&bytes)?);
+                let offsets = offsets(&bytes)?;
+                let start = read_header(offsets, string)?;
+                let end = read_header(offsets, size_of::<u32>() + string).unwrap_or(bytes.len());
+                let string_bytes = bytes
+                    .get(start..end)
+                    .ok_or(InvalidBytecode::MalformedHeader)?;
+                let string = str::from_utf8(string_bytes).map_err(InvalidBytecode::Utf8Error)?;
+                Ok(Rc::from(string))
+            })
+            .collect::<Result<_, Error>>()?;
+        Ok(Self {
+            bytes,
+            owned_strings,
+        })
+    }
+}
+
+impl Program {
+    pub fn eval<'host>(
+        &self,
+        block_id: usize,
+        stack: &mut Vec<Value<'host>>,
+    ) -> Result<Value<'host>, Error<'host>> {
+        struct Frame<'a> {
+            bytecode: &'a [u8],
+            pc: usize,
+        }
+
+        impl Frame<'_> {
+            fn next(&mut self) -> Result<u8, InvalidBytecode> {
+                let next = self
+                    .bytecode
+                    .get(self.pc)
+                    .ok_or(InvalidBytecode::ProgramOutOfBounds)?;
+                self.pc += 1;
+                Ok(*next)
+            }
+
+            fn next4(&mut self) -> Result<usize, InvalidBytecode> {
+                Ok(
+                    u32::from_le_bytes([self.next()?, self.next()?, self.next()?, self.next()?])
+                        as usize,
+                )
+            }
+
+            fn next_i64(&mut self) -> Result<i64, InvalidBytecode> {
+                Ok(i64::from_le_bytes([
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                    self.next()?,
+                ]))
+            }
+
+            fn pop<'host>(
+                // This doesn't use self yet, but i want to include pc in errors eventually.
+                &'_ self,
+                stack: &mut Vec<Value<'host>>,
+            ) -> Result<Value<'host>, Error<'host>> {
+                stack.pop().ok_or(InvalidBytecode::StackUnderflow.into())
+            }
+        }
+
+        let mut program = Frame {
+            bytecode: block(&self.bytes, block_id)?,
+            pc: 0,
+        };
+
+        // The program counter reaching the first (and only the first)
+        // out-of-bounds byte should be considered a return.
+        while program.pc != program.bytecode.len() {
+            macro_rules! bi_op {
+                (let $l:ident, $r:ident: $type:ident => $expr_type:ident: $expr:expr) => {{
+                    let $r = program.pop(stack)?;
+                    let $l = program.pop(stack)?;
+                    match (&$l.storage, &$r.storage) {
+                        (Storage::$type($l), Storage::$type($r)) => stack.push(Value {
+                            storage: Storage::$expr_type($expr),
+                        }),
+                        _ => return Err(Error::ExpectedNumbers($l, $r)),
+                    }
+                }};
+            }
+            macro_rules! bi_num {
+                (let $l:ident, $r:ident => $expr:expr) => {
+                    bi_op!(let $l, $r: I64 => I64: $expr)
+                };
+            }
+            macro_rules! bi_cmp {
+                (let $l:ident, $r:ident => $expr:expr) => {
+                    bi_op!(let $l, $r: I64 => Bool: $expr)
+                };
+            }
+            let instruction = program.next()?;
+            match instruction {
+                instruction::CLONE => {
+                    let index = program.next4()? as i32;
+                    match index {
+                        0.. => {
+                            let value = stack
+                                .get(index as usize)
+                                .ok_or(InvalidBytecode::StackOutOfBounds)?;
+                            stack.push(value.clone());
+                        }
+                        builtins::ANY => {
+                            stack.push(Type::Any.into());
+                        }
+                        builtins::UNIT => {
+                            stack.push(Type::Unit.into());
+                        }
+                        builtins::I64 => {
+                            stack.push(Type::I64.into());
+                        }
+                        builtins::OPTION => {
+                            stack.push(
+                                Storage::Function(Rc::new(FunctionAction::Option.into())).into(),
+                            );
+                        }
+                        builtins::MUT => {
+                            stack.push(
+                                Storage::Function(Rc::new(FunctionAction::Mut.into())).into(),
+                            );
+                        }
+                        _ => Err(InvalidBytecode::InvalidBuiltin)?,
+                    }
+                }
+                instruction::POP => {
+                    program.pop(stack)?;
+                }
+                instruction::COLLAPSE => {
+                    let value = program.pop(stack)?;
+                    for _ in 0..(stack.len() - program.next4()?) {
+                        stack.pop();
+                    }
+                    stack.push(value);
+                }
+                instruction::JUMP => {
+                    program.pc = program.next4()?;
+                }
+                instruction::IF => {
+                    let target = program.next4()?;
+                    if let Value {
+                        storage: Storage::Bool(false),
+                    } = program.pop(stack)?
+                    {
+                        program.pc = target;
+                    }
+                }
+
+                instruction::PUSH_UNIT => {
+                    stack.push(().into());
+                }
+                instruction::PUSH_TRUE => {
+                    stack.push(true.into());
+                }
+                instruction::PUSH_FALSE => {
+                    stack.push(false.into());
+                }
+                instruction::PUSH_I64 => {
+                    stack.push(program.next_i64()?.into());
+                }
+                instruction::PUSH_STRING => {
+                    let string_id = program.next4()?;
+                    let string = self
+                        .owned_strings
+                        .get(string_id)
+                        .ok_or(InvalidBytecode::UnexpectedStringId)?
+                        .clone();
+                    stack.push(string.into());
+                }
+                instruction::PUSH_FUNCTION => {
+                    let captures = program.next4()?;
+                    let function = program.next4()?;
+                    let output = program.pop(stack)?;
+                    let input = program.pop(stack)?;
+                    let new_stack = stack.split_off(stack.len() - captures);
+                    stack.push(
+                        Storage::Function(Rc::new(
+                            FunctionAction::With {
+                                program: self.clone(),
+                                signature: FunctionType {
+                                    input: input.try_into()?,
+                                    output: output.try_into()?,
+                                },
+                                block_id: function,
+                                captures: new_stack,
+                            }
+                            .into(),
+                        ))
+                        .into(),
+                    );
+                }
+                instruction::PUSH_ENUM => {
+                    let variants = program.pop(stack)?;
+                    let Value {
+                        storage: Storage::Tuple(Tuple(TupleStorage::Named(variants))),
+                    } = variants
+                    else {
+                        Err(Error::ExpectedNamedTuple(variants))?
+                    };
+                    let variants = rc_slice_try_from_iter(
+                        variants.len(),
+                        variants.iter().map(|(name, value)| {
+                            value.clone().try_into().map(|value| (name.clone(), value))
+                        }),
+                    )?;
+                    stack.push(Type::from(EnumType { variants }).into());
+                }
+
+                instruction::ADD => bi_num!(let l, r => l + r),
+                instruction::SUB => bi_num!(let l, r => l - r),
+                instruction::MUL => bi_num!(let l, r => l * r),
+                instruction::DIV => bi_num!(let l, r => l / r),
+                instruction::BITWISE_AND => bi_num!(let l, r => l & r),
+                instruction::BITWISE_OR => bi_num!(let l, r => l | r),
+                instruction::BITWISE_XOR => bi_num!(let l, r => l ^ r),
+                instruction::GREATER => bi_cmp!(let l, r => l > r),
+                instruction::GREATER_EQUAL => bi_cmp!(let l, r => l >= r),
+                instruction::LESSER => bi_cmp!(let l, r => l < r),
+                instruction::LESSER_EQUAL => bi_cmp!(let l, r => l <= r),
+                instruction::EQUAL_TO => {
+                    let r = program.pop(stack)?;
+                    let l = program.pop(stack)?;
+                    stack.push(l.eq(r)?.into());
+                }
+                instruction::NOT_EQUAL_TO => {
+                    let r = program.pop(stack)?;
+                    let l = program.pop(stack)?;
+                    stack.push((!l.eq(r)?).into());
+                }
+                instruction::LOGICAL_AND => bi_op!(let l, r: Bool => Bool: *l && *r),
+                instruction::LOGICAL_OR => bi_op!(let l, r: Bool => Bool: *l || *r),
+                instruction::PIPE => {
+                    let function = program.pop(stack)?;
+                    let argument = program.pop(stack)?;
+                    match function.storage {
+                        Storage::Function(mut function) => {
+                            let function_mut = Rc::make_mut(&mut function);
+                            let mut arguments = ().into();
+                            mem::swap(&mut arguments, &mut function_mut.argument);
+                            arguments = Value::concat(arguments, argument);
+                            mem::swap(&mut arguments, &mut function_mut.argument);
+                            stack.push(Storage::Function(function).into());
+                        }
+                        _ => return Err(Error::ExpectedFunction(function)),
+                    }
+                }
+
+                instruction::CALL => {
+                    let argument = program.pop(stack)?;
+                    let function = program.pop(stack)?;
+                    let result = match function {
+                        Value {
+                            storage: Storage::Function(function),
+                        } => Rc::<Function>::try_unwrap(function)
+                            .unwrap_or_else(|function| (*function).clone())
+                            .piped(argument)
+                            .eval()?,
+                        function => Err(Error::ExpectedFunction(function))?,
+                    };
+                    stack.push(result);
+                }
+                instruction::TUPLE => {
+                    let r = program.pop(stack)?;
+                    let l = program.pop(stack)?;
+                    stack.push(Value::concat(l, r));
+                }
+                instruction::INDEX => {
+                    let index = program.pop(stack)?;
+                    let container = program.pop(stack)?;
+                    stack.push(container.index(index)?);
+                }
+                instruction::NAME => {
+                    let name_id = program.next4()?;
+                    let name = self
+                        .owned_strings
+                        .get(name_id)
+                        .ok_or(InvalidBytecode::UnexpectedStringId)?
+                        .clone();
+                    let value = program.pop(stack)?;
+                    stack.push(Storage::Tuple(Tuple::from([(name, value)])).into())
+                }
+                instruction::NEST => {
+                    let value = program.pop(stack)?;
+                    stack.push(Storage::Tuple(Tuple::from([value])).into())
+                }
+                instruction::NEGATIVE => {
+                    let value = program.pop(stack)?.into_i64()?;
+                    stack.push((-value).into());
+                }
+                instruction::DEREF => {
+                    let value = program.pop(stack)?.into_refcell()?;
+                    stack.push(value.try_borrow()?.clone());
+                }
+                instruction::SET => {
+                    let value = program.pop(stack)?;
+                    let target = program.pop(stack)?.into_refcell()?;
+                    *target.borrow_mut() = value;
+                }
+
+                _ => Err(InvalidBytecode::InvalidInstruction)?,
+            }
+        }
+        program.pop(stack)
+    }
+}
+
+pub trait Extern {
+    fn index<'host>(&'host self, _index: Value<'host>) -> Result<Value<'host>, Error<'host>> {
+        Err(ExternError::MissingIndexImpl)?
+    }
+
+    fn debug(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{external value}}")
+    }
+}
+
+pub trait ExternFn {
+    fn call<'host>(&'host self, _argument: Value<'host>) -> Result<Value<'host>, Error<'host>> {
+        Err(ExternError::MissingFunctionImpl)?
+    }
+
+    fn debug(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{external function}}")
+    }
+}
+
+pub struct FunctionWrapper<F: for<'host> Fn(Value<'host>) -> Result<Value<'host>, Error<'host>>>(F);
+
+pub fn wrap_fn<F: for<'host> Fn(Value<'host>) -> Result<Value<'host>, Error<'host>>>(
+    f: F,
+) -> FunctionWrapper<F> {
+    FunctionWrapper::from(f)
+}
+
+impl<F> From<F> for FunctionWrapper<F>
+where
+    F: for<'host> Fn(Value<'host>) -> Result<Value<'host>, Error<'host>>,
+{
+    fn from(f: F) -> Self {
+        Self(f)
+    }
+}
+
+impl<F> ExternFn for FunctionWrapper<F>
+where
+    F: for<'host> Fn(Value<'host>) -> Result<Value<'host>, Error<'host>>,
+{
+    fn call<'host>(&self, argument: Value<'host>) -> Result<Value<'host>, Error<'host>> {
+        self.0(argument)
+    }
+}
+
+pub struct FunctionWrapperMut<
+    F: for<'host> FnMut(Value<'host>) -> Result<Value<'host>, Error<'host>>,
+>(RefCell<F>);
+
+pub fn wrap_fn_mut<F: for<'host> FnMut(Value<'host>) -> Result<Value<'host>, Error<'host>>>(
+    f: F,
+) -> FunctionWrapperMut<F> {
+    FunctionWrapperMut::from(f)
+}
+
+impl<F> From<F> for FunctionWrapperMut<F>
+where
+    F: for<'host> FnMut(Value<'host>) -> Result<Value<'host>, Error<'host>>,
+{
+    fn from(f: F) -> Self {
+        Self(RefCell::new(f))
+    }
+}
+
+impl<F> ExternFn for FunctionWrapperMut<F>
+where
+    F: for<'host> FnMut(Value<'host>) -> Result<Value<'host>, Error<'host>>,
+{
+    fn call<'host>(&self, argument: Value<'host>) -> Result<Value<'host>, Error<'host>> {
+        self.0.try_borrow_mut()?(argument)
     }
 }
